24e52dfa154bb670f9f9ab670e9b9cc9c466eb76
kromerh <HeikoKromer@user-goldens-macbook-pro.home>
2022-10-24 18:38:19 +0200

    simple flask

--- simple_flask.py
+++ simple_flask.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+
+
 
 app = Flask(__name__)
-@app.route('/')
+app.config['UPLOAD_FOLDER'] = 'static'
 
-def index():
-    return 'Hello world'
+@app.route('/')
+@app.route('/index')
+def show_index():
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'test.jpg')
+    return render_template("index.html", captured_photo = full_filename)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host='0.0.0.0')
+
+    
